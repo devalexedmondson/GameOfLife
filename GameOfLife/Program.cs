31c91ddb4efe6a124ceb6dd81b4b70e6cab79c25
devalexedmondson <alexedmondson20@gmail.com>
2017-01-18 17:12:42 -0600

    uploaded finished filed of GameOfLife

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adventure game = new Adventure();
+            game.RunGame();
+            Console.ReadKey();
+        }
+    }
+}
